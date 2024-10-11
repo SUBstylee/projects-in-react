637e322e22bf3b52e1606ffcd32e5655314bbaef
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-10-12 01:56:36 +0800

    steps complete date-counter intial commit

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+const messages = ['Steal underpants 🧺', '??? 🤔', 'Profit 🤑'];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
+
+	const handlePrevious = () => step > 1 && setStep((s) => s - 1);
+	const handleNext = () => step < 3 && setStep((s) => s + 1);
+
+	return (
+		<>
+			<button className='close' onClick={() => setIsOpen((io) => !io)}>
+				{isOpen ? String.fromCharCode(215) : String.fromCharCode(926)}
+			</button>
+			{isOpen && (
+				<div className='steps'>
+					<div className='numbers'>
+						<div className={`number ${step >= 1 ? 'active' : ''}`}>1</div>
+						<div className={`number ${step >= 2 ? 'active' : ''}`}>2</div>
+						<div className={`number ${step >= 3 ? 'active' : ''}`}>3</div>
+					</div>
+					<p className='message'>
+						Step {step}: {messages[step - 1]}
+					</p>
+					<div className='buttons'>
+						<button
+							onClick={handlePrevious}
+							style={{
+								backgroundColor: step === 1 ? '#999' : '#7950f2',
+								color: '#fff',
+							}}>
+							Previous
+						</button>
+						<button
+							onClick={handleNext}
+							style={{
+								backgroundColor: step === 3 ? '#999' : '#7950f2',
+								color: '#fff',
+							}}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default App;
