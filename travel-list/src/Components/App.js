d387a66835b030f413793583cfe1f757f47b89f3
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-10-15 22:15:34 +0800

    moved app to components

--- travel-list/src/Components/App.js
+++ travel-list/src/Components/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
-import Logo from './Components/Logo';
-import Form from './Components/Form';
-import PackingList from './Components/PackingList';
-import Stats from './Components/Stats';
+import Logo from './Logo';
+import Form from './Form';
+import PackingList from './PackingList';
+import Stats from './Stats';
 
 export default function App() {
 	const [items, setItems] = useState([]);
