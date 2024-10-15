752b294500e9d9ce4a7233f0be040f6f0b176bfc
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-10-15 22:12:25 +0800

    moved components

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+import Logo from './Components/Logo';
+import Form from './Components/Form';
+import PackingList from './Components/PackingList';
+import Stats from './Components/Stats';
+
 export default function App() {
 	const [items, setItems] = useState([]);
 
@@ -19,6 +24,14 @@ export default function App() {
 		);
 	};
 
+	const handleClearList = () => {
+		const confirmed = window.confirm(
+			'Are you sure you want to delete all items?',
+		);
+
+		if (confirmed) setItems([]);
+	};
+
 	return (
 		<div className='app'>
 			<Logo />
@@ -27,95 +40,9 @@ export default function App() {
 				items={items}
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
+				onClearList={handleClearList}
 			/>
 			<Stats items={items} />
 		</div>
 	);
 }
-
-const Logo = () => <h1>🏝️ Far Away 🧳</h1>;
-
-const Form = ({ onAddItems }) => {
-	const [description, setDescription] = useState('');
-	const [quantity, setQuantity] = useState(1);
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!description) return;
-		const newItem = { description, quantity, packed: false, id: Date.now() };
-		onAddItems(newItem);
-
-		setDescription('');
-		setQuantity(1);
-	};
-	return (
-		<form className='add-form' onSubmit={handleSubmit}>
-			<h3>What do you need for your 😍 trip?</h3>
-			<select
-				value={quantity}
-				onChange={(e) => setQuantity(Number(e.target.value))}>
-				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-					<option value={num} key={num}>
-						{num}
-					</option>
-				))}
-			</select>
-			<input
-				type='text'
-				placeholder='Item...'
-				value={description}
-				onChange={(e) => setDescription(e.target.value)}
-			/>
-			<button>Add</button>
-		</form>
-	);
-};
-
-const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
-	return (
-		<div className='list'>
-			<ul>
-				{items.map((item) => (
-					<Item
-						onDeleteItem={onDeleteItem}
-						onToggleItem={onToggleItem}
-						item={item}
-						key={item.id}
-					/>
-				))}
-			</ul>
-		</div>
-	);
-};
-
-const Item = ({ item, onDeleteItem, onToggleItem }) => (
-	<li>
-		<input
-			type='checkbox'
-			value={item.packed}
-			onChange={() => {
-				onToggleItem(item.id);
-			}}
-		/>
-		<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
-			{item.quantity} {item.description}
-		</span>
-		<button onClick={() => onDeleteItem(item.id)}>❌</button>
-	</li>
-);
-
-const Stats = ({ items }) => {
-	const numItems = items.length;
-	const numPacked = items.filter((item) => item.packed).length;
-	const percentPacked = Math.round((numPacked / numItems) * 100);
-
-	return (
-		<footer className='stats'>
-			<em>
-				{percentPacked === 100
-					? 'You got everything! Ready to go ✈️'
-					: `🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%)`}
-			</em>
-		</footer>
-	);
-};
---
+++ travel-list/src/Components/Stats.js
@@ -0,0 +1,24 @@
+const Stats = ({ items }) => {
+	if (!items.length)
+		return (
+			<p className='stats'>
+				<em>Start adding some items to your packing list 📋</em>
+			</p>
+		);
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const percentPacked = Math.round((numPacked / numItems) * 100);
+
+	return (
+		<footer className='stats'>
+			<em>
+				{percentPacked === 100
+					? 'You got everything! Ready to go ✈️'
+					: `🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%)`}
+			</em>
+		</footer>
+	);
+};
+
+export default Stats;
