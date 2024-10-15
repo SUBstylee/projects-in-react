00c722d7e118f93fe85cb86ab5ab0c92dcd6a00a
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-10-15 22:46:53 +0800

    accordion functionality

--- accordion-component/src/App.js
+++ accordion-component/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const faqs = [
@@ -18,11 +19,36 @@ const faqs = [
 export default function App() {
 	return (
 		<div>
-			<Accordion />
+			<Accordion data={faqs} />
 		</div>
 	);
 }
 
-function Accordion() {
-	return <div>TODO</div>;
+function Accordion({ data }) {
+	return (
+		<div className='accordion'>
+			{data.map((el, i) => (
+				<AccordionItem
+					key={el.title}
+					num={i + 1}
+					title={el.title}
+					text={el.text}
+				/>
+			))}
+			;
+		</div>
+	);
+}
+
+function AccordionItem({ num, title, text }) {
+	const [isOpen, setIsOpen] = useState(false);
+	const handleToggle = () => setIsOpen(!isOpen);
+	return (
+		<div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+			<p className='number'>{num < 10 ? `0${num}` : num}</p>
+			<p className='title'>{title}</p>
+			<p className='icon'>{isOpen ? '-' : '+'}</p>
+			{isOpen && <div className='content-box'>{text}</div>}
+		</div>
+	);
 }
