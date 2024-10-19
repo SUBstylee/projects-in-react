24271a66cbdb1b08f9a09c448e28e2e907562304
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-10-20 00:28:43 +0800

    split out components

---
+++ eat-n-split/src/Components/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Button from './Button';
+import FriendsList from './FriendsList';
+import FormAddFriend from './FormAddFriend';
+import FormSplitBill from './FormSplitBill';
+
+import { initialFriends } from '../consts/data';
+
+const App = () => {
+	const [showAddFriend, setShowAddFriend] = useState(false);
+	const [friends, setFriends] = useState(initialFriends);
+	const [selectedFriend, setSelectedFriend] = useState(null);
+
+	const handleShowAddFriend = () => {
+		setShowAddFriend((show) => !show);
+		setSelectedFriend(null);
+	};
+
+	const handleAddFriend = (friend) => {
+		setFriends((friends) => [...friends, friend]);
+		setShowAddFriend((show) => !show);
+	};
+
+	const handleSelection = (friend) => {
+		setSelectedFriend(friend);
+		setShowAddFriend(false);
+	};
+
+	const handleSplitBill = (value) => {
+		setFriends(
+			friends.map((friend) =>
+				friend.id === selectedFriend.id
+					? { ...friend, balance: friend.balance + value }
+					: friend,
+			),
+		);
+		setSelectedFriend(null);
+	};
+
+	return (
+		<div className='app'>
+			<div className='sidebar'>
+				<FriendsList
+					friends={friends}
+					selectedFriend={selectedFriend}
+					onSelection={handleSelection}
+				/>
+				{showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+				<Button onClick={handleShowAddFriend}>
+					{showAddFriend ? 'Close' : 'Add Friend'}
+				</Button>
+			</div>
+			{selectedFriend && (
+				<FormSplitBill
+					selectedFriend={selectedFriend}
+					onSplitBill={handleSplitBill}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default App;
