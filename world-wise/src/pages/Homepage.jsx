d58be4f3553e938faf6834971e7c7f7952bfca77
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-11-01 02:50:55 +0800

    moved starter

---
+++ world-wise/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+
+const Homepage = () => {
+	return (
+		<div>
+			<PageNav />
+			<h1 className='test'>WorldWise</h1>
+			<Link to='app'>Go to the app</Link>
+		</div>
+	);
+};
+
+export default Homepage;
---
+++ world-wise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+const PageNotFound = () => {
+	return (
+		<div>
+			<PageNav />
+			<h1>PageNotFound</h1>
+		</div>
+	);
+};
+
+export default PageNotFound;
