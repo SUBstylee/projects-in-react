de7aca234ce837d5edc6547199cc97b3c60f209f
Jeremy Threlfall <jjthrelfall@gmail.com>
2024-11-02 08:43:24 +0800

    components cities mock api

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,18 +1,54 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+import CityList from './components/CityList';
+
+const BASE_URL = 'http://localhost:8000';
 
 const App = () => {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchCities = async () => {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch {
+				alert('Something went wrong with fetch!');
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Homepage />} />
+				<Route index element={<Homepage />} />
 				<Route path='product' element={<Product />} />
 				<Route path='pricing' element={<Pricing />} />
-				<Route path='app' element={<AppLayout />} />
+				<Route path='login' element={<Login />} />
+				<Route path='app' element={<AppLayout />}>
+					<Route
+						index
+						element={<CityList cities={cities} isLoading={isLoading} />}
+					/>
+					<Route
+						path='cities'
+						element={<CityList cities={cities} isLoading={isLoading} />}
+					/>
+					<Route path='countries' element={<p>List of countries</p>} />
+					<Route path='form' element={<p>Form</p>} />
+				</Route>
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
--- world-wise/src/pages/PageNotFound.jsx
+++ world-wise/src/pages/PageNotFound.jsx
@@ -1,7 +1,16 @@
+import PageNav from '../components/PageNav';
+// Uses the same styles as Product
+import styles from './Product.module.css';
+
 export default function PageNotFound() {
-  return (
-    <div>
-      <h1>Page not found 😢</h1>
-    </div>
-  );
+	return (
+		<main className={styles.product}>
+			<PageNav />
+			<section>
+				<div>
+					<h1>Page not found 😢</h1>
+				</div>
+			</section>
+		</main>
+	);
 }
